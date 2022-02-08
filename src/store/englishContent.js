7ed307158568b95a8111609e3b577f3612e12420
tomasscattini <tomasscattini@gmail.com>
2022-02-08 16:00:38 +0100

    Feature: Add text object for landing, profile, settings pages on englishContent.js

--- src/store/englishContent.js
+++ src/store/englishContent.js
@@ -6,17 +6,126 @@ const englishContent = {
         creatorWebsite: 'https://smartstudios.io/'
     },
     content: {
-        authPage: {
-            title: 'Auth page',
-            description: 'This is the Auth page'
+        landingPage: {
+            descriptionTitle: 'Some description for the platform',
+            steps: [],
+            authCard: {
+                loginTitle: 'Login',
+                registerTitle: 'Register',
+                forgotPwdTitle: 'Reset Password',
+                goBackToLoginLink: 'Go back to Login',
+                alreadyHaveAccountText: 'Already have an account?',
+                alreadyHaveAccountLink: 'Login',
+                forgotPwdText: 'Have you forgotten your password?',
+                forgotPwdLink: 'Reset Password',
+                dontHaveAccountText: "Don't have an account?",
+                dontHaveAccountLink: 'Register',
+                loginForm: {
+                    emailLabel: 'Email',
+                    passwordLabel: 'Password',
+                    submitBtn: 'Log in',
+                    emailRequired: 'You must enter an email',
+                    validEmail: 'You must enter a valid email',
+                    passwordRequired: 'Please enter your password.'
+                },
+                forgotPwdForm: {
+                    emailLabel: 'Email',
+                    submitBtn: 'Send reset link',
+                    emailRequired: 'You must enter an email',
+                    validEmail: 'You must enter a valid email'
+                },
+                registerForm: {
+                    fullNameLabel: 'Full name',
+                    emailLabel: 'Email',
+                    passwordLabel: 'Password',
+                    confirmPwdLabel: 'Confirm Password',
+                    termsText: 'I Accept the',
+                    termsLink: 'Terms & Conditions',
+                    submitBtn: 'Register',
+                    nameRequired: 'You must enter display name',
+                    emailRequired: 'You must enter an email',
+                    validEmail: 'You must enter a valid email',
+                    passwordRequired: 'Please enter your password.',
+                    validPassword: 'Password is too short - should be 8 chars minimum.',
+                    passwordMatch: 'Passwords must match',
+                    termsRequired: 'Please accept our Terms & Conditions'
+                },
+                mailConfirmationMessage: {
+                    title: 'We sent you an E-mail!',
+                    sentLinkTo: 'We have sent a link to',
+                    yourEmailDefault: 'your E-mail.',
+                    checkInbox: 'Check your inbox and click on the "Reset password" link to update your credentials.',
+                    didntReceiveEmailText: "Didn't receive the E-mail?",
+                    didntReceiveEmailLink: 'Re-send',
+                    goBackToLoginLink: 'Go back to Login',
+                    mailResentMessage: 'The E-mail has been re-sent'
+                }
+            }
         },
         privacyPolicy: {
             title: 'Privacy Policy',
-            content: 'This is where the privacy policy will be displayed'
+            content: 'This is where the privacy policy will go'
+        },
+        profilePage: {
+            editBtn: 'Edit',
+            cancelBtn: 'Cancel',
+            saveBtn: 'Save',
+            unsavedChangesMessage: 'There are Some Unsaved Changes. Do you want to go Away?',
+            unsavedChangesActionBtn: 'Leave',
+            generalTab: {
+                title: 'General',
+                keyInformation: {
+                    title: 'Key Information',
+                    firstNameLabel: 'First Name(s)',
+                    firstNamePlaceholder: 'Eg: John',
+                    lastNameLabel: 'Last Name(s)',
+                    lastNamePlaceholder: 'Eg: Doe',
+                    emailLabel: 'Email',
+                    emailPlaceholder: 'Eg: john@doe.com',
+                    phoneLabel: 'Phone number',
+                    phonePlaceholder: '+39 (351) 548 1875'
+                },
+                otherInformation: {
+                    title: 'Other'
+                }
+            },
+            otherTab: {
+                title: 'Other',
+                generalInformation: {
+                    title: 'General'
+                },
+                moreInformation: {
+                    title: 'More'
+                }
+            }
+        },
+        settingsPage: {
+            menu: {
+                title: 'Settings',
+                profileLink: 'Edit Profile',
+                changePwdLink: 'Change Password',
+                deleteAccountLink: 'Delete Account'
+            },
+            changePasswordCard: {
+                title: 'Change your password',
+                newPasswordLabel: 'New Password',
+                passwordConfirmLabel: 'Password (Confirm)',
+                submitBtn: 'Change my password',
+                newPasswordRequired: 'Please enter your new password.',
+                validPassword: 'Password is too short - should be 8 chars minimum.',
+                passwordMatch: 'Passwords must match.'
+            },
+            deleteAccountCard: {
+                title: 'Delete your account',
+                deleteLabel: 'Write "Delete"',
+                submitBtn: 'Delete Account',
+                confirmRequired: 'Please write exactly the word "Delete".',
+                confirmationWord: 'delete'
+            }
         },
         termsAndConditions: {
-            title: 'Terms and conditions',
-            content: 'This is where the terms and conditions will be displayed'
+            title: 'Terms & Conditions',
+            content: 'This is where the terms and conditions will go'
         }
     },
     footerdata: {
---
+++ workingon.md
@@ -0,0 +1,8 @@
+### v0.7.0
+
+-   Feature: Add isPreferredThemeCheckTriggered & isThemeToggable with reducers to uiSlice.
+-   Feature: Add check on themeProvider for isPreferredThemeCheckTriggered.
+-   Feature: Add conditional rendering of theme Switch on Navbar for isThemeToggable.
+-   Bugfix: Add default value for onSubmit on SignupForm.
+-   Feature: Add terms checkbox on SignupForm.
+-   Feature: Move all text to englishContent.js file.
